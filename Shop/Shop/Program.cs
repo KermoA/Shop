92ec197b2eac003ebcced24a82852243ef5cef77
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-09-23 11:41:51 +0300

    SpaceshipsIndex

--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+
 namespace Shop
 {
 	public class Program
@@ -9,6 +12,10 @@ namespace Shop
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
 
+			builder.Services.AddDbContext<ShopContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+			
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
