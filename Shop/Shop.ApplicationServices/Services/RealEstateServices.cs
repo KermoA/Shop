8f0a6eb6504528521c81a678bf112c9d2c175c5d
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-10-15 01:45:17 +0300

    RealEstatesDelete

--- Shop/Shop.ApplicationServices/Services/RealEstateServices.cs
+++ Shop/Shop.ApplicationServices/Services/RealEstateServices.cs
@@ -61,5 +61,14 @@ namespace Shop.ApplicationServices.Services
 
             return domain;
         }
+
+        public async Task<RealEstate> Delete(Guid id)
+        {
+            var realEstate = await _context.RealEstates
+                .FirstOrDefaultAsync(x => x.Id == id);
+            _context.RealEstates.Remove(realEstate);
+            await _context.SaveChangesAsync();
+            return realEstate;
+        }
     }
 }
